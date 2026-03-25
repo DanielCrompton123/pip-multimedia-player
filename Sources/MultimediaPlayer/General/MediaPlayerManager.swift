61b4947193a535fef1e1f2677628ce84574bd96a
Daniel Crompton <danielcrompton5@gmail.com>
2026-03-25 12:18:10 +0000

    Create the PiPVideoPlayer that takles in an AVPlayer and plays it in an AVPlayerViewController. Make sure that errors for PiP are printed out.

--- Sources/MultimediaPlayer/General/MediaPlayerManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  MultimediaPlayer
-//
-//  Created by daniel on 24/03/2026.
-//
-
-import Foundation
--- Sources/MultimediaPlayer/PiPVideoPlayer.swift
+++ Sources/MultimediaPlayer/PiPVideoPlayer.swift
@@ -6,17 +6,58 @@
 //
 
 import SwiftUI
-import UIKit
+import AVKit
 
 
-/// A video player that starts inline and can be expanded to be played in full screen
 public struct PiPVideoPlayer: UIViewControllerRepresentable {
-    public func makeUIViewController(context: Context) -> some UIViewController {
-        <#code#>
+    
+    private let player: AVPlayer
+    
+    public init(player: AVPlayer) {
+        self.player = player
+    }
+    
+    private let playerController = AVPlayerViewController()
+    
+    public func makeUIViewController(context: Context) -> AVPlayerViewController {
+        // Set up the view controller to begin with
+        playerController.player = player
+        playerController.allowsPictureInPicturePlayback = true
+        playerController.canStartPictureInPictureAutomaticallyFromInline = true
+        playerController.delegate = context.coordinator
+        
+        // Set up the category for the audio session
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .moviePlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("[PiPVideoPlayer.makeUIViewController]: Cannot set audio session to playback movies & active: \(error)")
+        }
+        
+        return playerController
+    }
+    
+    // Remember: Called when the CALLEE swiftUI view updates the state that this view depends on
+    public func updateUIViewController(_ playerViewController: AVPlayerViewController, context: Context) {
+        
     }
     
     
-    public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        <#code#>
+    // MARK: Coordinator
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    public class Coordinator: NSObject, AVPlayerViewControllerDelegate {
+        
+        public func playerViewController(
+            _ playerViewController: AVPlayerViewController,
+            failedToStartPictureInPictureWithError error: any Error
+        ) {
+            print("[PiPViewoPlayer.playerViewControllerFailedToStartPictureInPictureWithError]: Error = \(error)")
+        }
+        
     }
 }
+ 
